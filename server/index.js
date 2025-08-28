3d92013e474218c715a2d13532f04b3837eced2d
gd <gd@callibri.ru>
2025-08-28 14:09:38 +0500

    докер файлы

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import * as dotenv from 'dotenv';
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
